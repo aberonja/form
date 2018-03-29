98db48e49515e3ea8f46ce688e691439e7c1d514
Aleksandar Beronja <aberonja@execom.eu>
2018-03-29 11:57:41 +0200

    form style

--- form.js
+++ form.js
@@ -1,3 +1,4 @@
 console.log('------------------------------------');
-console.log(document.body.scrollHeight);
+console.log(document.documentElement.clientHeight);
+console.log(document.documentElement.scrollHeight);
 console.log('------------------------------------');
