181740afa7ac0d52faa8087bea439ccaad58dfce
Aleksandar Beronja <aberonja@execom.eu>
2018-03-29 12:47:24 +0200

    form style

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 console.log('------------------------------------');
 console.log(document.body.clientHeight);
 console.log(document.documentElement.scrollHeight);
-var width = document.document.getElementsByClassName('bodyContainer').offsetHeight;
+var width = document.document.getElementsById('inf_form_95d000ea8290e0e6a12b2d1af0fc6ca5').offsetHeight;
 console.log(width);
 console.log('------------------------------------');
