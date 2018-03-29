10aef29070f7d8fcb305598f3f0c221d6423ba65
Aleksandar Beronja <aberonja@execom.eu>
2018-03-29 17:55:59 +0200

    form style

--- form.js
+++ form.js
@@ -1,22 +1,4 @@
-// console.log(document.forms[0].getElementsByName('table')[1].offsetHeight)
-
-// setTimeout(() => {
-//     var height = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
-//     console.log(height)
-// }, 5000);
-// var height = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
-// console.log(height)
-
-// console.log('test')
-
-// var wn = window.frames[0]
-// wn.postMessage(height, 'http://localhost:8084/');
-
-// wn.postMessage(height, 'http://localhost:8084/');
-
-
 document.addEventListener("DOMContentLoaded", function() {
     var height = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
-    console.log(height)
     window.parent.postMessage(height, '*');
   });
