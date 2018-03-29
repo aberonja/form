f0a13f683543ea445e419e44557c8ca46acf7186
Aleksandar Beronja <aberonja@execom.eu>
2018-03-29 15:59:48 +0200

    form style

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
-console.log(document.forms[0].getElementsByName('table')[1].offsetHeight)
+// console.log(document.forms[0].getElementsByName('table')[1].offsetHeight)
 
-var height = document.forms[0].getElementsByName('table')[1].offsetHeight;
+var height = document.forms[0].getElementsByTagName('table')[1].offsetHeight;
 
 targetWindow.postMessage(height, Window.parent);
