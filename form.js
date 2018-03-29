9e7c50377f718f80ec38875947f2b9134fcbee44
Aleksandar Beronja <aberonja@execom.eu>
2018-03-29 16:30:50 +0200

    form style

--- form.js
+++ form.js
@@ -2,10 +2,10 @@
 
 setTimeout(() => {
     var height = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
+    console.log(height)
 }, 5000);
 
 console.log('test')
-console.log(height)
 
 // var wn = window.frames[0]
 // wn.postMessage(height, 'http://localhost:8084/');
