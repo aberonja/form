c5229527d22038c5e188a772cf83153774b7381c
Aleksandar Beronja <aberonja@execom.eu>
2018-03-30 09:13:45 +0200

    form js updated

--- form.js
+++ form.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var height = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
+    var errorHeght = document.getElementById('webformErrors').offsetHeight;
+    var formHeight = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
+    var height = errorHeght + formHeight;
     window.parent.postMessage(height, '*');
   });
