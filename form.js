b59a7a95057e8b9b9ca59dc300b79010a5a25ec1
Aleksandar Beronja <aberonja@execom.eu>
2018-03-30 16:09:26 +0200

    js changes

--- form.js
+++ form.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var errorHeght = document.getElementById('webformErrors').offsetHeight;
-    var formHeight = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight;
-    var height = errorHeght + formHeight;
-    window.parent.postMessage(height, '*');
-    document.getElementsByTagName('body')[0].classList.add('overflow');
-    console.log(height)
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var errorHeght = document.getElementById('webformErrors').offsetHeight
+  var formHeight = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight
+  var height = errorHeght + formHeight
+  document.getElementsByTagName('body')[0].classList.add('overflow')
+  console.log(window.location.href)
+  var test = {
+    origin: window.location.href,
+    message: {
+      type: 'HEIGHT',
+      data: height
+    }
+  }
+  window.parent.postMessage(test, '*')
+  console.log(test)
+});
