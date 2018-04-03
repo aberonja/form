3b9201977da6a99eb93d3b713759a609267917ef
Aleksandar Beronja <aberonja@execom.eu>
2018-04-03 14:59:12 +0200

    test

--- form.js
+++ form.js
@@ -2,10 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   var errorHeght = document.getElementById('webformErrors').offsetHeight
   var formHeight = document.forms[0].getElementsByClassName('webFormBodyContainer')[0].offsetHeight
   var height = errorHeght + formHeight
-  document.getElementsByClassName("infusion-submit")[0].getElementsByTagName("button")[0].onclick = function(){
-    var test = 1;
-    window.parent.postMessage(test, '*')
-  }
   document.getElementsByTagName('body')[0].classList.add('overflow')
   var iframeData = {
     origin: window.location.href,
@@ -16,5 +12,3 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   window.parent.postMessage(iframeData, '*')
 });
-
-console.log('testic')
