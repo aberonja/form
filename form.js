5a31b7ea662eefb6552c5a5f2a6cc8ef6d0e7675
Aleksandar Beronja <aberonja@execom.eu>
2018-04-23 14:46:51 +0200

    form test

--- form.js
+++ form.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("testici");
+  console.log("test");
   var formHeight = 0;
+  var messageHeight = 0;
+  var errorHeght = 0;
   if (document.getElementById("webformErrors") != undefined) {
-    var errorHeght = document.getElementById("webformErrors").offsetHeight;
+    errorHeght = document.getElementById("webformErrors").offsetHeight;
+  } else {
+    messageHeight = document.getElementsByClassName("bodyContainer")[0]
+      .offsetHeight;
   }
   if (document.getElementsByClassName("webFormBodyContainer").length != 0) {
     formHeight = document.forms[0].getElementsByClassName(
       "webFormBodyContainer"
     )[0].offsetHeight;
   }
-  var height = errorHeght + formHeight;
+  var height = errorHeght + formHeight + messageHeight;
   document.getElementsByTagName("body")[0].classList.add("overflow");
   var iframeData = {
     origin: window.location.href,
