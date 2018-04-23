a86578eff2b2b915fe1ad56357a708b718afc63d
Aleksandar Beronja <aberonja@execom.eu>
2018-04-23 14:40:57 +0200

    form test

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("test");
+  console.log("testici");
   var formHeight = 0;
   if (document.getElementById("webformErrors") != undefined) {
     var errorHeght = document.getElementById("webformErrors").offsetHeight;
   }
-  if (document.forms.getElementsByClassName("webFormBodyContainer")) {
+  if (document.getElementsByClassName("webFormBodyContainer").length != 0) {
     formHeight = document.forms[0].getElementsByClassName(
       "webFormBodyContainer"
     )[0].offsetHeight;
