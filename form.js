3dbd5a53864f795f6f7e72b8667786036bd114f1
Aleksandar Beronja <aberonja@execom.eu>
2018-05-24 11:41:02 +0200

    console log

--- form.js
+++ form.js
@@ -1,26 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   var formHeight = 0;
   var messageHeight = 0;
   var errorHeght = 0;
-  if (document.getElementById("webformErrors") != undefined) {
-    errorHeght = document.getElementById("webformErrors").offsetHeight;
+  if (document.getElementById('webformErrors') != undefined) {
+    errorHeght = document.getElementById('webformErrors').offsetHeight;
   } else {
-    messageHeight = document.getElementsByClassName("bodyContainer")[0]
+    messageHeight = document.getElementsByClassName('bodyContainer')[0]
       .offsetHeight;
   }
-  if (document.getElementsByClassName("webFormBodyContainer").length != 0) {
+  if (document.getElementsByClassName('webFormBodyContainer').length != 0) {
     formHeight = document.forms[0].getElementsByClassName(
-      "webFormBodyContainer"
+      'webFormBodyContainer'
     )[0].offsetHeight;
   }
   var height = errorHeght + formHeight + messageHeight;
-  document.getElementsByTagName("body")[0].classList.add("overflow");
+  document.getElementsByTagName('body')[0].classList.add('overflow');
   var iframeData = {
     origin: window.location.href,
     data: {
-      type: "HEIGHT",
+      type: 'HEIGHT',
       height: height
     }
   };
-  window.parent.postMessage(iframeData, "*");
+  window.parent.postMessage(iframeData, '*');
 });
+
+
+var KEYCODE_ESC = 27;
+
+    $(document).keyup(function (e) {
+      if (e.keyCode == KEYCODE_ESC) {
+        console.log('------------------------------------');
+        console.log('test');
+        console.log('------------------------------------');
+      }
+    });
